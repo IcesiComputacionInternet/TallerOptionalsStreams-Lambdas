40906b2aa46d0849d51cde36ebd4451cdddd37c9
GabrielSB19 <gasuarez2002@gmail.com>
2023-02-07 15:48:36 -0500

    Stream Example done the point 2

--- src/main/java/co/edu/icesi/functional/StreamExample.java
+++ src/main/java/co/edu/icesi/functional/StreamExample.java
@@ -3,9 +3,11 @@ package co.edu.icesi.functional;
 import co.edu.icesi.model.IcesiUser;
 import co.edu.icesi.model.SimpleName;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 
@@ -18,8 +20,10 @@ public class StreamExample {
      * @return a sorted list of different lastnames.
      */
     public List<String> allDifferentLastNamesSorted(List<IcesiUser> icesiUsers) {
-        
-        return null;
+        return icesiUsers.stream().filter(Objects::nonNull)
+                .map(IcesiUser::getLastName).flatMap(lastName -> Arrays.stream(lastName.split(" ")))
+                .sorted().distinct().collect(Collectors.toList());
+
     }
 
     /**
