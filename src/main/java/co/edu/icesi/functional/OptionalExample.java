62bc601ac1301b173537b3bbf62690f09de1a55a
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2023-02-07 22:42:41 -0500

    All tests working

--- src/main/java/co/edu/icesi/functional/OptionalExample.java
+++ src/main/java/co/edu/icesi/functional/OptionalExample.java
@@ -7,36 +7,39 @@ import co.edu.icesi.model.IcesiUser;
 
 public class OptionalExample {
 
-
     /**
      * Using optionals return the street of the given user if not present throw an
      * RuntimeException with the message "Couldn't get the street"
-     *  value = 0.5
+     * value = 0.5
+     * 
      * @param icesiUser an IcesiUser object
      * @return The street of the given icesiUser
      */
     public String obtainUserStreet(IcesiUser icesiUser) {
-        Optional<String> optStreet = Optional.ofNullable(icesiUser).map(IcesiUser::getAddress).map(IcesiAddress::getStreet);
-        if (optStreet.isEmpty()) {
-            optStreet.orElseThrow( () -> "Couldn't get the street");
-        } else {
-            optStreet.orElse(icesiUser.getAddress().getStreet());
-
-        }
-        //String result = optStreet.orElse("Couldn't get the street");
+        String result = Optional.ofNullable(icesiUser)
+                .map(us -> us.getAddress())
+                .map(ad -> ad.getStreet())
+                .orElseThrow(() -> new RuntimeException("Couldn't get the street"));
+        return result;
     }
-
 
     /**
-     * Using Optionals return the description of the given user if not present return
+     * Using Optionals return the description of the given user if not present
+     * return
      * a default description "default description"
-     *  value = 0.5
+     * value = 0.5
+     * 
      * @param icesiUser icesiUser an IcesiUser object
-     * @return the description of the given icesiUser or "default description" if not present.
+     * @return the description of the given icesiUser or "default description" if
+     *         not present.
      */
     public String obtainUserDescription(IcesiUser icesiUser) {
-        return null;
-    }
+        String desc = Optional.ofNullable(icesiUser)
+                .map(us -> us.getAddress())
+                .map(ad -> ad.getDescription())
+                .orElse("default description");
 
+        return desc;
+    }
 
 }
--- src/main/java/co/edu/icesi/functional/StreamExample.java
+++ src/main/java/co/edu/icesi/functional/StreamExample.java
@@ -4,26 +4,37 @@ import co.edu.icesi.model.IcesiUser;
 import co.edu.icesi.model.SimpleName;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 public class StreamExample {
 
     /**
-     * given a list of IcesiUser's get all the different lastnames if is a composed lastname each one of them
-     * will count as a individual lastname. example "Prada Calderon" counts as "Prada" and "Calderon"
+     * given a list of IcesiUser's get all the different lastnames if is a composed
+     * lastname each one of them
+     * will count as a individual lastname. example "Prada Calderon" counts as
+     * "Prada" and "Calderon"
      * value = 1.0
      *
      * @param icesiUsers a list of IcesiUser, can contain null values.
      * @return a sorted list of different lastnames.
      */
     public List<String> allDifferentLastNamesSorted(List<IcesiUser> icesiUsers) {
-        List<String> users = icesiUsers.stream().flatMap(s -> Arrays.stream(s.split(" ")))
+        List<String> newLastNames = icesiUsers.stream().filter(user -> user != null && user.getLastName() != null)
+                .map(icesiUser -> icesiUser.getLastName())
+                .flatMap(lastNames -> Arrays.stream(lastNames.split(" ")))
+                .sorted().distinct().toList();
+
+        return newLastNames;
 
     }
 
     /**
-     * given a list of IcesiUser's filter the list by the ones that match the street name with
+     * given a list of IcesiUser's filter the list by the ones that match the street
+     * name with
      * the street in his IcesiAddress.
      * value 1.0
      *
@@ -32,25 +43,29 @@ public class StreamExample {
      * @return a list of IcesiUser with the matching IcesiUser street.
      */
     public List<IcesiUser> filterUsersByStreet(List<IcesiUser> icesiUsers, String street) {
-        List<IcesiUser> s = icesiUsers.stream().filter(user -> user.getAddress()!= null && user.getAddress().getStreet() != null && user.getAddress().getStreet().equals(street)).toList();
+        List<IcesiUser> s = icesiUsers.stream().filter(user -> user.getAddress() != null
+                && user.getAddress().getStreet() != null && user.getAddress().getStreet().equals(street)).toList();
         return s;
     }
 
     /**
      * given a list of IcesiUser's map the names to a SimpleName class.
-     *  value = 0.5
+     * value = 0.5
+     * 
      * @param icesiUsers icesiUsers a list of IcesiUser, can contain null values.
      * @return a list of SimpleName.
      */
     public List<SimpleName> mapToSimpleName(List<IcesiUser> icesiUsers) {
-        List<String> names = icesiUsers.stream().map(IcesiUser::getFirstName).toList();
-        List<String> lastNames = icesiUsers.stream().map(IcesiUser::getLastName).toList();
+        List<String> names = icesiUsers.stream().filter(us -> us != null && us.getFirstName() != null)
+                .map(IcesiUser::getFirstName).toList();
+        List<String> lastNames = icesiUsers.stream().filter(us -> us != null && us.getLastName() != null)
+                .map(IcesiUser::getLastName).toList();
         List<SimpleName> simple = new ArrayList<>();
         for (int i = 0; i < names.size(); i++) {
             simple.add(new SimpleName(names.get(i), lastNames.get(i)));
 
         }
         return simple;
-        //List<SimpleName> simpeNames = Stream.concat(names.stream
+        // List<SimpleName> simpeNames = Stream.concat(names.stream
     }
 }
